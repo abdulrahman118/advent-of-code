3a645934a5196e5e551fc0b83f0531482308fdb3
Abdul Rahman <developer.abdulrahman@gmail.com>
2023-12-02 13:25:28 +0530

    Day1 part two

--- 2023/Day1/Day1.cs
+++ 2023/Day1/Day1.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode
 {
     internal class Day1
     {
-        public static int CalculateCaliberation()
+        public static int CalculateCaliberationPart1()
         {
             string path = $"{AppDomain.CurrentDomain.BaseDirectory}input.txt";
             string[] lines = File.ReadAllLines(path);
@@ -44,6 +44,74 @@ namespace AdventOfCode
             return sum;
         }
 
+        public static int CalculateCaliberationPart2()
+        {
+            string path = $"{AppDomain.CurrentDomain.BaseDirectory}input.txt";
+            string[] lines = File.ReadAllLines(path);
+            var stringToNumber = new Dictionary<string, int>();
+            var numberString = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
+            FillNumbers(stringToNumber);
+
+            int sum = 0;
+
+            foreach (string line in lines)
+            {
+                var indexList = new List<IndexValue>();
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (int.TryParse(line[i].ToString(), out int result))
+                    {
+                        indexList.Add(new IndexValue { Index = i, Value = result });
+                        break;
+                    }
+                }
+
+                for (int i = line.Length - 1; i >= 0; i--)
+                {
+                    if (int.TryParse(line[i].ToString(), out int result))
+                    {
+                        indexList.Add(new IndexValue { Index = i, Value = result });
+                        break;
+                    }
+                }
+
+                foreach (string number in numberString)
+                {
+                    if (line.Contains(number))
+                    {
+                        indexList.Add(new IndexValue { Index = line.IndexOf(number), Value = stringToNumber[number] });
+                        indexList.Add(new IndexValue { Index = line.LastIndexOf(number), Value = stringToNumber[number] });
+                    }
+                }
+
+                indexList = indexList.OrderBy(i => i.Index).ToList();
+
+                sum += int.Parse($"{indexList.First().Value}{indexList.Last().Value}");
+            }
+
+            // 53866.
+            return sum;
+        }
+
+        private static void FillNumbers(Dictionary<string, int> numberStrings)
+        {
+            numberStrings.Add("one", 1);
+            numberStrings.Add("two", 2);
+            numberStrings.Add("three", 3);
+            numberStrings.Add("four", 4);
+            numberStrings.Add("five", 5);
+            numberStrings.Add("six", 6);
+            numberStrings.Add("seven", 7);
+            numberStrings.Add("eight", 8);
+            numberStrings.Add("nine", 9);
+        }
+    }
+
+    internal class IndexValue
+    {
+        public int Index { get; set; }
+        public int Value { get; set; }
     }
 }
